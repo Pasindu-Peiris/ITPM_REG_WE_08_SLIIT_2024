7acbe63cc89d675f571368673193c48bbc8035ce
Pasindu Peiris <it21831454@my.sliit.lk>
2024-03-06 18:28:01 +0530

    Add Images

--- client/src/component/clientside/Nav.jsx
+++ client/src/component/clientside/Nav.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className=''>
+    <div className='Nav'>
+
+        <div className="logo">
+            <img src={{}} alt="" />
+        </div>
 
 
     </div>
