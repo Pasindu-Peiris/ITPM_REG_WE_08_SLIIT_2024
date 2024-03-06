d87656579aed8ea138809be619578bfc977b1327
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2024-03-06 22:38:15 +0530

    fetched new code

--- client/src/component/clientside/HsectionOne.jsx
+++ client/src/component/clientside/HsectionOne.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from 'react';
+
+
 
 const HsectionOne = () => {
+
+    const addImg = {
+        width: "100%",
+        height:"100vh",
+        backgroundImage:"url(" +{}+")"
+    }
+
+
+
   return (
 
 
-    <div>
+    <div className='flex w-100 h-100' style={addImg} >
 
 
 
     </div>
 
-    
+
   )
 }
 
