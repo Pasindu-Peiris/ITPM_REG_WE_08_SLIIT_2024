d537ed1166c41e4201e371d575e78c49aa7344ff
Pasindu Peiris <it21831454@my.sliit.lk>
2024-03-07 23:18:57 +0530

    Update HSectionThird.jsx

--- client/src/component/clientside/HSectionThird.jsx
+++ client/src/component/clientside/HSectionThird.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Img from '../../Images/top-bg.jpg'
+import Img1 from '../../Images/icon1.png'
+import Img2 from '../../Images/icon2.png'
+import Img3 from '../../Images/icon3.png'
 
 const HSectionThird = () => {
 
@@ -8,7 +11,7 @@ const HSectionThird = () => {
         height: "80vh",
         backgroundImage: `url(${Img})`,
         backgroundSize: "cover",
-        backgroundPosition:"top"
+        backgroundPosition: "top"
     }
 
 
@@ -17,6 +20,45 @@ const HSectionThird = () => {
         <div className='flex items-center justify-center  text-gray-100' style={addImg}>
 
 
+            <div className="main-block grid grid-cols-3 gap-5 text-black w-[90%] ">
+
+                <div className="block-1 block items-center justify-center text-center m-2">
+
+                    <div className="block-img flex items-center justify-center p-3">
+                        <img src={Img1} alt="" width={140} className='' />
+                    </div>
+
+                    <h1 className='text-2xl font-semibold p-2'>15 Years of Experiences</h1>
+
+                    <p className='p-1 text-xl'>Host our community of good-natured campers, glampers, and RV travelers on your land or at your cabin.</p>
+
+                </div>
+
+                <div className="block-1 block items-center justify-center text-center  m-2">
+
+                    <div className="block-img flex items-center justify-center p-3">
+                        <img src={Img2} alt="" width={140} className='' />
+                    </div>
+
+                    <h1 className='text-2xl font-semibold p-2'>200+ Camps To Visit</h1>
+
+                    <p className='p-1 text-xl'>Host our community of good-natured campers, glampers, and RV travelers on your land or at your cabin.</p>
+
+                </div>
+
+                <div className="block-1 block items-center justify-center text-center  m-2">
+
+                    <div className="block-img flex items-center justify-center p-3">
+                        <img src={Img3} alt="" width={140} className='' />
+                    </div>
+
+                    <h1 className='text-2xl font-semibold p-2'>Big Community</h1>
+
+                    <p className='p-1 text-xl'>Host our community of good-natured campers, glampers, and RV travelers on your land or at your cabin.</p>
+
+                </div>
+
+            </div>
 
 
         </div>
