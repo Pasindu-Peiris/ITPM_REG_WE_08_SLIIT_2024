6899f5b7e498828ec66a7146d88d0b264d1edfad
Pasindu Peiris <it21831454@my.sliit.lk>
2024-03-11 21:05:59 +0530

    Map updated

--- client/src/component/clientside/LocMap.jsx
+++ client/src/component/clientside/LocMap.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { GoogleMap, Marker, MarkerF, useJsApiLoader } from '@react-google-maps/api';
-import LocIcon from '../../Images/map-location.png'
+import LocIcon from '../../Images/loc.png'
 
 const LocMap = () => {
 
@@ -12,42 +12,62 @@ const LocMap = () => {
     })
 
     const markers = [
-        
         {
-          id: 2,
-          name: "sivali",
-          position: {lat:6.8486 , lng:80.2600},
+            id: 1,
+            name: "eheliaygoda",
+            position: { lat: 6.7056, lng: 80.3847 }
         },
         {
-          id:3,
-          name:"kuruwita",
-          position:{lat:6.8218, lng:80.3615}
+            id: 2,
+            name: "sivali",
+            position: { lat: 6.8486, lng: 80.2600 },
+        },
+        {
+            id: 3,
+            name: "kuruwita",
+            position: { lat: 6.8218, lng: 80.3615 }
+        },
+        {
+            id: 4,
+            name: "homagama",
+            position: { lat: 6.8433, lng: 80.0032 }
+        },
+        {
+            id: 5,
+            name: "horana",
+            position: { lat: 6.7230, lng: 80.0647 }
+        },
+        {
+            id: 6,
+            name: "Nivithigala",
+            position: { lat: 6.5959, lng: 80.4578 }
         }
-      
-      ]
+
+    ]
 
 
     return (
         <div className=' w-[100%] h-[90vh] flex justify-center items-center'>
-            {isLoaded ?<GoogleMap
-                mapContainerStyle={ {width:"100%", height:"90vh"}}
+            {isLoaded ? <GoogleMap
+                mapContainerStyle={{ width: "100%", height: "90vh" }}
                 center={
-                    {lat:6.8486, lng:80.2600}
+                    { lat: 6.8486, lng: 80.2600 }
                 }
                 zoom={10}
 
-               
+
             >
 
                 {markers.map(({ id, name, position }) => (
-                        <MarkerF key={id} position={position} icon={{
-                            
+                    <MarkerF key={id} position={position} icon={{
+                        url: `${LocIcon}`,
+                        scaledSize: { width: 50, height: 50 },
 
-                        }} onClick={() => console.log(`${name}`)}></MarkerF>
-                    ))
-                    
+                    }} onClick={() => console.log(`${name}`)}></MarkerF>
+                ))
+
                 }
-            </GoogleMap> :null
+            </GoogleMap> : null
 
             }
         </div>
