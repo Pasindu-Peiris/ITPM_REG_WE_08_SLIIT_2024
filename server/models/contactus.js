5f682c846560e7236763c0a4748de2df5ee62583
IT21826740 <IT21826740@my.sliit.lk>
2024-03-17 10:59:03 +0530

    create models

---
+++ server/models/contactus.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const contactusSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+});
+
+const Contactus = mongoose.model("Contactus", contactusSchema);
+module.exports = Contactus;
