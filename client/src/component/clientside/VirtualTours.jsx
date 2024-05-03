5df671b630e2a0422e0d0f6c19ee93f292d6e0f3
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2024-05-03 12:18:28 +0530

    completed virtual tours UI

--- client/src/component/clientside/VirtualTours.jsx
+++ client/src/component/clientside/VirtualTours.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
 
-
-
 const VirtualTours = () => {
   const [images, setImages] = useState([]);
 
@@ -12,7 +10,6 @@ const VirtualTours = () => {
       try {
         const response = await fetch("http://localhost:8090/api/images/all");
         const data = await response.json();
-        console.log(data);
 
         const imagesData = data.map((item) => ({
           _id: item._id,
@@ -20,6 +17,7 @@ const VirtualTours = () => {
             item.imagePaths && item.imagePaths.length > 0
               ? item.imagePaths[0]
               : item.imagePath,
+          title: item.title, // Include the title in the mapped object
         }));
         setImages(imagesData);
       } catch (error) {
@@ -32,20 +30,24 @@ const VirtualTours = () => {
   return (
     <div>
       <Nav />
-      <div className="mt-36">
-        <h1 className="text-center text-4xl font-semibold text-black">
-          Virtual Tours
-        </h1>
+      <div className="mt-12 text-center">
+        <h1 className="text-4xl font-semibold text-gray-900">Virtual Tours</h1>
       </div>
-      <div class="mt-4 flex justify-center flex-wrap">
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {images.map((image, index) => (
-          <div class=" w-80 h-80 relative overflow-hidden" key={index}>
+          <div
+            className="relative rounded-lg overflow-hidden shadow-md hover:shadow-lg"
+            key={index}
+          >
             <Link to={`/view/${image._id}`}>
               <img
                 src={`http://localhost:8090/${image.imagePath}`}
                 alt=""
-                className="absolute h-72 w-72 object-cover transition-transform duration-300 ease-in-out transform hover:scale-110 rounded-lg"
+                className="w-full h-52 object-cover transition duration-300 ease-in-out transform hover:scale-110"
               />
+              <div className="absolute bottom-0 left-0 right-0 bg-gray-900 bg-opacity-75 text-white p-4">
+                <p className="text-lg font-semibold">{image.title}</p>
+              </div>
             </Link>
           </div>
         ))}
