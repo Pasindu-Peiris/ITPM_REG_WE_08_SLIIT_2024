e18cf78688319afd2bc1734c5b8ddde98b873807
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2024-05-03 12:51:41 +0530

    Update Profile.jsx

--- client/src/component/clientside/Profile.jsx
+++ client/src/component/clientside/Profile.jsx
@@ -77,7 +77,7 @@ const Profile = () => {
               </div>
             </div>
           ) : (
-            <p className="text-lg font-semibold text-center">You haven't logged in </p>
+            <p className="text-lg font-semibold text-center">Loading... </p>
           )}
         </div>
       </Card>
